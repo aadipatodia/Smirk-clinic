1fc0506ec8d929d96ae6a22b3fd0d792501db42c
Aadi <patodiaaadi@gmail.com>
2026-04-17 00:47:06 +0530

    base url

--- smirk-dental/backend/server.js
+++ smirk-dental/backend/server.js
@@ -17,7 +17,7 @@ const { startScheduler } = require('./services/scheduler');
 const appointmentsRouter = require('./routes/appointments');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const unavailableRouter = require('./routes/unavailable');
 app.use('/unavailable', unavailableRouter);
